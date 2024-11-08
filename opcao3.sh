50b3b3b12d2b99ea28f830c8e5914eb3268f03c9
Lucas <lucas2002mkx@gmail.com>
2024-11-08 18:07:11 -0300

    WIP: Adição de cores e adição da senha do usuário

--- opcao3.sh
+++ opcao3.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+# Definição de cores
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+YELLOW='\033[1;33m'
+CYAN='\033[0;36m'
+WHITE='\033[1;37m'
+NC='\033[0m' 
+
 exibir_menu_opcao_3(){
-    echo "Gerenciamento de Usuários - Escolha uma opção:"
-    echo "1. Adicionar novo usuário"
+    echo -e "${YELLOW}Escolha uma opção para o gerenciamento de usuários:${NC}"
+    echo -e "${WHITE}1. Adicionar novo usuário"
     echo "2. Remover um usuário"
     echo "3. Listar todos os usuários"
-    echo "4. Sair"
+    echo -e "4. Sair${NC}"
 }
 
 while true; do
@@ -16,7 +25,11 @@ while true; do
             # Adicionar novo usuário
             read -p "Digite o nome do novo usuário: " novo_usuario
             sudo useradd "$novo_usuario" && echo "Usuário '$novo_usuario' adicionado com sucesso!" || echo "Erro ao adicionar o usuário."
-            #  TODO adicionar senha para usuário
+            read -p "Digite a senha do novo usuário: " senha_novo_usuario
+            echo "$novo_usuario:$senha_novo_usuario" | sudo chpasswd
+            sudo mkdir -p /home/"$novo_usuario"
+            sudo chown "$novo_usuario":"$novo_usuario" /home/"$novo_usuario"
+            sudo chmod 700 /home/"$novo_usuario"
             ;;
         2)
             # Remover um usuário
@@ -40,9 +53,11 @@ while true; do
             ;;
     esac
 
-    # Pergunta se o usuário deseja voltar ao menu principal
-    read -p "Deseja voltar ao menu principal? (s/n): " voltar
-    if [[ $voltar != "n" ]]; then
+    # Pergunta ao usuário se ele quer continuar
+    read -p "Deseja realizar outra ação? (s/n): " continuar
+    #Transformar em minúsculo o input
+    continuar=${continuar,,}
+    if [[ $continuar != "s" ]]; then
         echo "Encerrando o script."
         break
     fi
